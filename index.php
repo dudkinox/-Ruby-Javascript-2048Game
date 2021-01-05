2dd4bbde2657ad507c1fe98f309ee9384f4c4ee9
ศุภณัฐ ชินราช <45527907+dudkinox@users.noreply.github.com>
2021-01-05 14:43:35 +0700

    update

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>2048 online</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<div class="content">
+					<h1><a href="#">2048</a></h1>
+					<p>login ก่อนเล่นนะค้าบ<br />
+					แนะนำความเห็นเพิ่มเติมกับผู้สร้าง <a href="https://www.facebook.com/blind.vlltar" target="_blank">facebook</a></p>
+					<ul class="actions">
+						<li><input type = "text" id = "username" name = "username" placeholder="กรอกไอดี"/></li>
+						<li><input type = "password" id = "password" name = "password" placeholder="รหัส"/></li>
+          </ul>
+          <button class = "button primary" onclick="login()">เข้าสู่ระบบ</button>
+        </div>
+        
+				<div class="image phone"><div class="inner"><img src="images/screen.PNG" alt="" />&nbsp;&nbsp;ลองรับมือถือ</div></div>
+			</header>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
